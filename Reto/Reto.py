efe6334747cb7c29a215018592aeb7523522bc96
Veronica Alexandra <veronica.micanquer@upb.edu.co>
2026-04-02 22:55:16 -0500

    Realizo cambios en reto.py

--- Reto/Reto.py
+++ Reto/Reto.py
@@ -1,5 +1,5 @@
 # Lista principal
-aeronaves = []    #Creamos una lista vacía en la que se alacenarán todas las aeronaves
+aeronaves = []    #Creamos una lista vacía en la que se almacenarán todas las aeronaves
 
 contador = 0
 
@@ -7,7 +7,7 @@ contador = 0
 # Para registrar una aeronave:
 while True:
     
-    print(f"\nAeronave {contador + 1}")         #Imprime la palabra aeronave junto al numero de aeronave que se está registrando
+    print(f"\nAeronave {contador + 1}")         #Imprime la palabra aeronave junto al número de aeronave que se está registrando
     
     #Pedimos la información de la aeronave al usuario
     matricula = input("Por favor ingrese la matricula de la aeronave: ")
@@ -19,16 +19,16 @@ while True:
         "matricula": matricula,
         "modelo": modelo,
         "horas_vuelo": horas_vuelo,
-        "componentes": []      #Se crea una lista vacía para que posteriormente pidamos los datos que se alamacenarán aquí
+        "componentes": []      #Se crea una lista vacía para que posteriormente pidamos los datos que se alacenarán aquí
     }
     
-    #Pedimos los datos que se alamacenarán el la lista componentes
+    #Pedimos los datos que se almacenarán el la lista componentes
     cantidad_componentes = int(input("Número de componentes: "))    #Le pedimos al usuario que ingrese el numero de componentes que desea registrar
     
     for i in range(cantidad_componentes):                           #Bucle for que se repite de acuerdo a la cantidad de componentes que el usuario ingrese
         print(f"\nComponente {i + 1}")
         
-        #Pedimos que inrgrese la información de los componentes
+        #Pedimos que ingrese la información de los componentes
         nombre = input("Por favor ingrese el nombre del componente: ")
         horas_uso = float(input("Por favor ingrese las horas de uso del componente: "))
         limite = float(input("Por favor ingrese el límite de horas: "))
@@ -41,26 +41,26 @@ while True:
         #Ese diccionario  se almacenará en la lista vacía componentes
         aeronave["componentes"].append(componente)
     
-    #Finalmente esa aeronave, con toda su información se almacenará en la lista aerolinea
+    #Se guarda la aeronave en la lista principal
     aeronaves.append(aeronave)
 
     #Termina un ciclo entonces se le suma uno
     contador += 1
     
-    # Minimo se deben ingresar tres aeronaves, entonces...
+    # Mínimo se deben ingresar tres aeronaves, entonces...
     if contador >= 3:
         opcion = input("\n¿Agregar otra aeronave? (si/no): ")
         if opcion == "no":
-            break
+            break     #Estaba en un ciclo infinito while, break rompe con ese bucle
 
 
 #Para el reporte..
 
 print("\nREPORTE DE MANTENIMIENTO")
 
-for aeronave in aeronaves:
-    print("\nAeronave:", aeronave["matricula"])
+for aeronave in aeronaves:     #Recorre toda la lista de aeronaves
+    print("\nAeronave:", aeronave["matricula"])    #Accede al valor de la clave matricula
     
-    for componente in aeronave["componentes"]:
-        if componente["horas_uso"] > componente["limite"]:
-            print("Mantenimiento:", componente["nombre"])
+    for componente in aeronave["componentes"]:    #Recorre la lista de componentes de la aeronave
+        if componente["horas_uso"] > componente["limite"]:   #Compara horas de uso con el límite
+            print("Mantenimiento:", componente["nombre"])    #Si las horas de uso son mayores a la limite imprime esto
